4878c6597ba373d3f49871598b7e5665425ca9b0
Michael Spitznagel <exultavit@gmail.com>
2023-05-09 16:42:04 -0400

    Extract int and long tags

--- apps/nbtshow.cpp
+++ apps/nbtshow.cpp
@@ -76,14 +76,57 @@ bool has_gzip_header(std::vector<unsigned char> &data) {
     return (data[0] == 0x1f) && (data[1] == 0x8b) && (data[2] == 0x08);
 }
 
+int32_t extract_int32(const std::vector<unsigned char> &data,
+                      std::vector<unsigned char>::const_iterator &iter) {
+    // Check if data has sufficient length
+    auto end_offset = data.end() - iter;
+    if (end_offset < 4) {
+        throw std::runtime_error("Insufficient data length");
+    }
+    return __builtin_bswap32(*reinterpret_cast<const int32_t *>(&(*iter)));
+
+    /*    int32_t result = (data[offset] << 24) | (data[offset + 1] << 16) |
+                         (data[offset + 2] << 8) | data[offset + 3];
+    */
+}
+
+int64_t extract_int64(const std::vector<unsigned char> &data,
+                      std::vector<unsigned char>::const_iterator &iter) {
+    // Check if data has sufficient length
+    auto end_offset = data.end() - iter;
+    if (end_offset < 8) {
+        throw std::runtime_error("Insufficient data length");
+    }
+
+    return __builtin_bswap64(*reinterpret_cast<const int64_t *>(&(*iter)));
+}
+
+void print_int_tag(std::vector<unsigned char> &data, const std::string &name) {
+    auto ret = nbt::fast_find_named_tag(data.begin(), data.end(),
+                                        nbt::tagtype::TAG_Int, name);
+    if (ret != data.end()) {
+        auto val = extract_int32(data, ret);
+        std::cout << "Found '" << name << " = " << val << " at "
+                  << ret - data.begin() << std::endl;
+    }
+}
+
+void print_long_tag(std::vector<unsigned char> &data, const std::string &name) {
+    auto ret = nbt::fast_find_named_tag(data.begin(), data.end(),
+                                        nbt::tagtype::TAG_Long, name);
+    if (ret != data.end()) {
+        auto val = extract_int64(data, ret);
+        std::cout << "Found '" << name << " = " << val << " at "
+                  << ret - data.begin() << std::endl;
+    }
+}
+
 int main(int argc, const char *argv[]) {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " filename search-key"
-                  << std::endl;
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " filename" << std::endl;
         return -1;
     }
     std::string filename(argv[1]);
-    std::string key(argv[2]);
 
     auto bytes = read_file(filename);
     if (bytes.size() < 8) {
@@ -93,15 +136,17 @@ int main(int argc, const char *argv[]) {
 
     if (has_gzip_header(bytes)) {
         bytes = decompress_gzip(bytes);
-    } 
+    }
 
-    auto ret = nbt::fast_find_named_tag(bytes.begin(), bytes.end(),
-                                        nbt::tagtype::TAG_Int, key);
-    if (ret == bytes.end()) {
-        std::cout << "Did not find: '" << key << "'" << std::endl;
-    } else {
-        std::cout << "Found '" << key << "' at " << ret - bytes.begin()
-                  << std::endl;
+    std::vector<std::string> int_tags{"SpawnX", "SpawnY", "SpawnZ",
+                                      "playerGameType", "DataVersion"};
+    for (auto tag_name : int_tags) {
+        print_int_tag(bytes, tag_name);
+    }
+
+    std::vector<std::string> long_tags{"RandomSeed", "LastPlayed", "Time"};
+    for (auto tag_name : long_tags) {
+        print_long_tag(bytes, tag_name);
     }
 
     return 0;
