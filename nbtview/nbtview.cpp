a1dfb341816616e88793d68e667f9ef6168168e1
Michael Spitznagel <exultavit@gmail.com>
2023-05-11 14:13:25 -0400

    Test BinaryScanner on floating point values

--- nbtview/nbtview.cpp
+++ nbtview/nbtview.cpp
@@ -82,7 +82,7 @@ InputIterator emplace_tag(InputIterator input_start, InputIterator input_stop,
 #ifdef TEST_NBTVIEW_INTERNALS
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("nbtview::BinaryScanner") {
+TEST_CASE("nbtview::BinaryScanner Integer Types") {
     auto v =
         std::vector<uint8_t>{0x17, 0x23, 0x01, 0xff, 0x01, 0x23, 0x45, 0x67};
     auto s = nbtview::BinaryScanner(v);
@@ -102,4 +102,14 @@ TEST_CASE("nbtview::BinaryScanner") {
     REQUIRE(c.value() == 0x01ff);
     REQUIRE(d.value() == 0x01234567);
 }
+
+TEST_CASE("nbtview::BinaryScanner Floating Point Types") {
+    auto v = std::vector<uint8_t>{0xc3, 0x78, 0xc0, 0x00};
+    auto s = nbtview::BinaryScanner(v);
+    auto a = s.get_value<float>();
+
+    REQUIRE(a != std::nullopt);
+    REQUIRE(a.value() == -248.75);
+}
+
 #endif
