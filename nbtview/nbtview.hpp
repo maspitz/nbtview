f4357d1b9083c3466be751977eaa6a468537d1db
Michael Spitznagel <exultavit@gmail.com>
2023-05-25 11:25:25 -0400

    Complete accessor interface for Compound
    get_ptr<T> is created to look up unique_ptr<T> in the variant map
    and return a bare pointer if found.
    
    The reason for the bare pointer
    is that the accessor is provided only a view, not ownership in any way.
    unique_ptr is uncopyable for a reason, and passing a const reference to
    would have no advantage over a bare pointer.  A nullptr return
    signifies that the name was either not found, or else was found but
    not of type T.
    
    get_opt<T> is renamed to get_value<T> to contrast with the meaning
    of get_ptr<T>.  This is, it returns a value rather than a reference.
    
    get_List, get_Compound, get_Byte_Array, etc. are all implemented
    using get_ptr<T>.

--- nbtview/nbtview.hpp
+++ nbtview/nbtview.hpp
@@ -97,51 +97,56 @@ struct List : public Tag {
 };
 
 struct Compound : public Tag {
+
+  public:
     std::map<std::string, payload_type> data;
     Compound() : Tag(TypeCode::Compound) {}
 
-    template <typename T>
-    std::optional<T> get_opt(const std::string &name) const {
-        auto it = data.find(name);
-        if (it == data.end()) {
-            return std::nullopt;
-        }
-        if (!std::holds_alternative<T>(it->second)) {
-            return std::nullopt;
-        }
-        return std::get<T>(it->second);
-    }
-
     std::optional<Byte> get_Byte(const std::string &name) const {
-        return get_opt<Byte>(name);
+        return get_value<Byte>(name);
     }
 
     std::optional<Short> get_Short(const std::string &name) const {
-        return get_opt<Short>(name);
+        return get_value<Short>(name);
     }
 
     std::optional<Int> get_Int(const std::string &name) const {
-        return get_opt<Int>(name);
+        return get_value<Int>(name);
     }
 
     std::optional<Long> get_Long(const std::string &name) const {
-        return get_opt<Long>(name);
+        return get_value<Long>(name);
     }
 
     std::optional<Float> get_Float(const std::string &name) const {
-        return get_opt<Float>(name);
+        return get_value<Float>(name);
     }
 
     std::optional<Double> get_Double(const std::string &name) const {
-        return get_opt<Double>(name);
+        return get_value<Double>(name);
     }
 
-    const Compound *get_Compound(const std::string &name) const {
-        return std::get<std::unique_ptr<Compound>>(data.at(name)).get();
+    const Byte_Array *get_Byte_Array(const std::string &name) const {
+        return get_ptr<Byte_Array>(name);
     }
 
     std::optional<String> get_String(const std::string &name) const {
-        return get_opt<String>(name);
+        return get_value<String>(name);
+    }
+    const List *get_List(const std::string &name) const {
+        return get_ptr<List>(name);
+    }
+
+    const Compound *get_Compound(const std::string &name) const {
+        return get_ptr<Compound>(name);
+    }
+
+    const Int_Array *get_Int_Array(const std::string &name) const {
+        return get_ptr<Int_Array>(name);
+    }
+
+    const Long_Array *get_Long_Array(const std::string &name) const {
+        return get_ptr<Long_Array>(name);
     }
 
     std::string to_string() {
@@ -153,6 +158,30 @@ struct Compound : public Tag {
         // }
         oss << "]";
         return oss.str();
+    }
+
+  private:
+    template <typename T>
+    std::optional<T> get_value(const std::string &name) const {
+        auto it = data.find(name);
+        if (it == data.end()) {
+            return std::nullopt;
+        }
+        if (!std::holds_alternative<T>(it->second)) {
+            return std::nullopt;
+        }
+        return std::get<T>(it->second);
+    }
+
+    template <typename T> const T *get_ptr(const std::string &name) const {
+        auto it = data.find(name);
+        if (it == data.end()) {
+            return nullptr;
+        }
+        if (!std::holds_alternative<std::unique_ptr<T>>(it->second)) {
+            return nullptr;
+        }
+        return std::get<std::unique_ptr<T>>(it->second).get();
     }
 };
 
