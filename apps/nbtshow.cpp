fb4113afde9955d1c63491c568261898bf2a128b
Michael Spitznagel <exultavit@gmail.com>
2023-05-26 16:55:46 -0400

    Exit nicely if file can't be opened

--- apps/nbtshow.cpp
+++ apps/nbtshow.cpp
@@ -60,6 +60,13 @@ std::vector<unsigned char> read_file(const std::string &filename) {
     // Get the file size
     std::ifstream file(filename,
                        std::ios::binary | std::ios::in | std::ios::ate);
+
+    if (!file) {
+        std::cerr << "File " << filename << " could not be opened."
+                  << std::endl;
+        exit(0);
+    }
+
     auto size = file.tellg();
     file.seekg(0, std::ios::beg);
 
