15d4fbbb854edf46f131a9c861a97212bd1f69d1
Michael Spitznagel <exultavit@gmail.com>
2023-05-26 17:05:14 -0400

    Print root tag data as string

--- apps/nbtshow.cpp
+++ apps/nbtshow.cpp
@@ -159,8 +159,8 @@ int main(int argc, const char *argv[]) {
 
     auto bscr = nbtview::BinaryScanner(bytes);
 
-    // auto root_tag = nbtview::make_tag(bscr);
-    // std::cout << "root_tag: " << root_tag->to_string() << std::endl;
+    auto root_tag = nbtview::make_tag_root(bscr);
+    std::cout << "root_tag: " << root_tag->to_string() << std::endl;
 
     return 0;
 }
