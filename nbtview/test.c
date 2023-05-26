7a33e388f5cca63433ecb96069c7bbbaf97d4543
Michael Spitznagel <exultavit@gmail.com>
2023-05-26 14:07:19 -0400

    Remove unneeded test file

--- nbtview/test.c
+++
@@ -1,9 +0,0 @@
-/* Some test data for nbtview */
-
-/* Unnamed empty compound followed by TAG end
-char* nbt_1 = { 0x0a, 0x0, 0x0, 0x0 };
-/* Compound named 'Data' */
-char* nbt_2 = { 0x0a, 0x0, 0x4, 'D', 'a', 't', 'a', 0x0 };
-
-
-
