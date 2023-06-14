5db293a82f0030e64310fa58aaced224ed3a0a74
Michael Spitznagel <exultavit@gmail.com>
2023-06-14 17:41:59 -0400

    Revise comment

--- nbtview/BinaryScanner.cpp
+++ nbtview/BinaryScanner.cpp
@@ -4,10 +4,8 @@
 
 namespace nbtview {
 
-// Really, only BinaryScanner::get_string is a candidate for the source file.
-
-// The other BinaryScanner member functions are all template functions, and so
-// they must go into the header file so that the linker can find their
-// instantiations.
+// The implementations of the BinaryScanner templated member functions
+// must go into the header file so that the linker can find their
+// instantiations.  So that's why there's nothing in here...
 
 } // namespace nbtview
