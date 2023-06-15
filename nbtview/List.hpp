e2b66da61aa0b23ed3d8f9224e3d1b0234c737a5
Michael Spitznagel <exultavit@gmail.com>
2023-06-15 14:16:35 -0400

    Remove unneeded header #includes
    Since List.hpp doesn't define anything it itself, it only really needs
    to include "List_fwd.hpp".  No other headers or forward declarations are
    needed at this time.

--- nbtview/List.hpp
+++ nbtview/List.hpp
@@ -2,17 +2,9 @@
 #ifndef NBT_LIST_H_
 #define NBT_LIST_H_
 
-#include <string>
-#include <vector>
-
 #include "List_fwd.hpp"
-#include "Tag.hpp"
-#include "nbtview.hpp"
 
 namespace nbtview {
-
-class BinaryScanner;
-class Tag;
 
 // Any templated methods on List that must appear in a header file should go
 // here, just like in Compound.hpp.  (There aren't any yet.)
