716bbe571cfdde20b96289d3452bd7d613d0ed0e
Michael Spitznagel <exultavit@gmail.com>
2023-06-26 16:41:09 -0400

    Create tag_utilities files

---
+++ nbtview/tag_utilities.cpp
@@ -0,0 +1,2 @@
+// tag_utilities.cpp
+
---
+++ nbtview/tag_utilities.hpp
@@ -0,0 +1,10 @@
+// tag_utilities.hpp
+
+#ifndef TAG_UTILITIES_H
+#define TAG_UTILITIES_H
+
+namespace nbtview {
+
+} // namespace nbtview
+
+#endif // TAG_UTILITIES_H
