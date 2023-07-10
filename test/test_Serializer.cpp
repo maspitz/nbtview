9935346e42133bb5219ed3b68e63cad663b0e14e
Michael Spitznagel <exultavit@gmail.com>
2023-07-10 16:21:59 -0400

    Test string and compound tag serialization

--- test/test_Serializer.cpp
+++ test/test_Serializer.cpp
@@ -16,14 +16,13 @@ namespace nbt = nbtview;
 
 namespace doctest {
 
-template <> struct StringMaker<std::vector<unsigned char>> {
-    static String convert(const std::vector<unsigned char> &in) {
+template <> struct StringMaker<std::vector<char>> {
+    static String convert(const std::vector<char> &in) {
         std::ostringstream oss;
 
         oss << "[";
         oss << std::hex << std::setfill('0');
-        for (typename std::vector<unsigned char>::const_iterator it =
-                 in.begin();
+        for (typename std::vector<char>::const_iterator it = in.begin();
              it != in.end(); ++it)
             oss << "0x" << std::setw(2) << static_cast<unsigned int>(*it)
                 << ", ";
@@ -57,6 +56,26 @@ TEST_CASE("nbtview::Serializer functions") {
                   std::vector<char>{static_cast<char>(nbt::TypeCode::Byte),
                                     0x00, 0x06, 'M', 'y', 'B', 'y', 't', 'e',
                                     0x17});
+        }
+    }
+
+    SUBCASE("Serialize string tags") {
+        nbt::Tag empty_compound(nbt::String{"Hello"});
+        nbt::Serializer::serialize(empty_compound, "Tag", output);
+        auto v_root_string_tag =
+            std::vector<char>{0x08, 0x00, 0x03, 'T', 'a', 'g', 0x00,
+                              0x05, 'H',  'e',  'l', 'l', 'o'};
+
+        CHECK(stream_chars(output) == v_root_string_tag);
+    }
+
+    SUBCASE("Serialize compound tags") {
+        SUBCASE("Empty compound tag (unnamed)") {
+            nbt::Tag empty_compound(nbt::Compound{});
+            nbt::Serializer::serialize(empty_compound, "", output);
+            auto v_empty_compound_tag =
+                std::vector<char>{0x0a, 0x00, 0x00, 0x00};
+            CHECK(stream_chars(output) == v_empty_compound_tag);
         }
     }
 }
