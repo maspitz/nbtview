ec8f37efc745a6fd132aad0ca5a3b17098cdbe6b
Michael Spitznagel <exultavit@gmail.com>
2023-07-11 14:05:55 -0400

    Test serialization of nested Compound tag

--- test/test_Serializer.cpp
+++ test/test_Serializer.cpp
@@ -79,7 +79,7 @@ TEST_CASE("nbtview::Serializer functions") {
             auto v_empty_compound_tag = std::vector<char>{
                 0x09,                   // TypeCode::List
                 0x00, 0x01,             // Name length
-                0x41,                   // "A"
+                'A',                    // "A"
                 0x04,                   // TypeCode::Long
                 0x00, 0x00, 0x00, 0x02, // list length of 2
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, // 10L
@@ -92,11 +92,57 @@ TEST_CASE("nbtview::Serializer functions") {
 
     SUBCASE("Serialize compound tags") {
         SUBCASE("Empty compound tag (unnamed)") {
-            nbt::Tag empty_compound(nbt::Compound{});
+            nbt::Compound empty_compound;
             nbt::Serializer::serialize(empty_compound, "", output);
             auto v_empty_compound_tag =
                 std::vector<char>{0x0a, 0x00, 0x00, 0x00};
             CHECK(stream_chars(output) == v_empty_compound_tag);
+        }
+        nbt::Compound ham;
+        ham.emplace("Hampus", nbt::Float(0.75));
+        nbt::Compound egg;
+        egg.emplace("Eggbert", nbt::Float(0.50));
+        auto v_ham = std::vector<char>{
+            0x0a,                                       // Compound
+            0x00, 0x03, 'h',  'a',  'm',                // "ham"
+            0x05,                                       // Float
+            0x00, 0x06, 'H',  'a',  'm', 'p', 'u', 's', // "Hampus"
+            0x3f, 0x40, 0x00, 0x00,                     // 0.75 [IEEE 754]
+            0x00                                        // End
+        };
+        auto v_egg = std::vector<char>{
+            0x0a,                                            // Compound
+            0x00, 0x03, 'e',  'g',  'g',                     // "egg"
+            0x05,                                            // Float
+            0x00, 0x07, 'E',  'g',  'g', 'b', 'e', 'r', 't', // "Eggbert"
+            0x3f, 0x00, 0x00, 0x00,                          // 0.50
+            0x00                                             // End
+        };
+        SUBCASE("Compound tag with float #1") {
+            nbt::Serializer::serialize(ham, "ham", output);
+            CHECK(stream_chars(output) == v_ham);
+        }
+        SUBCASE("Compound tag with float #2") {
+            nbt::Serializer::serialize(egg, "egg", output);
+            CHECK(stream_chars(output) == v_egg);
+        }
+        SUBCASE("Nested Compound tag") {
+            nbt::Compound nested;
+            nested.put("ham", std::move(ham));
+            nested.put("egg", std::move(egg));
+            auto v_nested = std::vector<char>{
+                0x0a,                                    // Compound
+                0x00, 0x06, 'n', 'e', 's', 't', 'e', 'd' // "nested"
+            };
+            // Because we are using std::map with a std::string key,
+            // Compounds will be serialized with their elements appearing
+            // in lexicographic order.  That is, "egg" comes before "ham".
+            v_nested.insert(v_nested.end(), v_egg.begin(), v_egg.end());
+            v_nested.insert(v_nested.end(), v_ham.begin(), v_ham.end());
+            v_nested.push_back(0x00); // End
+
+            nbt::Serializer::serialize(nested, "nested", output);
+            CHECK(stream_chars(output) == v_nested);
         }
     }
 }
