d114106ddd2ab56b5d87f61060afe11f813e0c01
Michael Spitznagel <exultavit@gmail.com>
2023-07-31 12:01:17 -0400

    Don't advance stream reading position for region
    It made sense to allow reading timestamps and offsets to
    advance the stream as a side effect, but if we don't advance,
    it becomes simpler for the client to read chunk data, and
    the order of metadata reading is not as important.

--- nbtview/Region.cpp
+++ nbtview/Region.cpp
@@ -9,16 +9,20 @@ namespace nbtview {
 
 std::istream &Region::ReadTimestamps(std::istream &input) {
     std::vector<unsigned char> bytes(SECTOR_LENGTH);
+    auto init_pos = input.tellg();
+    input.seekg(init_pos + SECTOR_LENGTH, std::ios::beg);
     input.read(reinterpret_cast<char *>(bytes.data()), SECTOR_LENGTH);
     BinaryReader reader(std::move(bytes));
     for (int i = 0; i < N_CHUNKS; ++i) {
         chunk[i].timestamp = reader.get_value<uint32_t>();
     }
+    input.seekg(init_pos, std::ios::beg);
     return input;
 }
 
 std::istream &Region::ReadOffsets(std::istream &input) {
     std::vector<unsigned char> bytes(SECTOR_LENGTH);
+    auto init_pos = input.tellg();
     input.read(reinterpret_cast<char *>(bytes.data()), SECTOR_LENGTH);
     BinaryReader reader(std::move(bytes));
     for (int i = 0; i < N_CHUNKS; ++i) {
@@ -26,6 +30,7 @@ std::istream &Region::ReadOffsets(std::istream &input) {
         chunk[i].length = data & 0xff;
         chunk[i].offset = data >> 8;
     }
+    input.seekg(init_pos, std::ios::beg);
     return input;
 }
 
