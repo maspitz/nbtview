2ae3911c413a487e7e995d9262edef6de793d3bc
maspitz <maspitz@users.noreply.github.com>
2023-07-31 20:31:56 +0000

    deploy: 957187e3917e4ea7aee3973ac12b16dc4c8d3271

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['get_18',['get',['../classnbtview_1_1Compound.html#aa3e34fe1fd2f862e82e135c8c3a8a1fe',1,'nbtview::Compound::get()'],['../classnbtview_1_1List.html#a906094b701bde3e845ebea87850ce355',1,'nbtview::List::get()']]],
-  ['get_5fchunk_5fbytes_19',['get_chunk_bytes',['../classnbtview_1_1Region.html#aea281f422660b5242b53095442398aa5',1,'nbtview::Region']]],
+  ['get_5fchunk_5fdata_19',['get_chunk_data',['../classnbtview_1_1Region.html#a90f2a446cbbd4b59f018f0b315ec16cf',1,'nbtview::Region']]],
   ['get_5flength_20',['get_length',['../classnbtview_1_1Region.html#a8035ff01e728793559fab17d47bba936',1,'nbtview::Region']]],
   ['get_5foffset_21',['get_offset',['../classnbtview_1_1Region.html#abee66b2e244577fe42767d5aff25e640',1,'nbtview::Region']]],
   ['get_5ftimestamp_22',['get_timestamp',['../classnbtview_1_1Region.html#abce2d5bbedf25e15e52eea9f08f03a33',1,'nbtview::Region']]]
