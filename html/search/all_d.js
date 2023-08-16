d2ce8801d7d42e7f11695d76287b3165f0332856
maspitz <maspitz@users.noreply.github.com>
2023-08-16 15:39:56 +0000

    deploy: 70e69f4cd908fb8607b324cb940d1ca27b4f1593

--- html/search/all_d.js
+++ html/search/all_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['sector_5flength_40',['SECTOR_LENGTH',['../classnbtview_1_1Region.html#a438234847870d7f6817b262fd8cf2f58',1,'nbtview::Region']]],
+  ['sector_5flength_40',['sector_length',['../classnbtview_1_1Region.html#ab7be249b44ee81041e76dc17e9f557b9',1,'nbtview::Region']]],
   ['serializer_2ehpp_41',['Serializer.hpp',['../Serializer_8hpp.html',1,'']]],
   ['short_42',['Short',['../namespacenbtview.html#a135ffec411a1c14822713fd40959fdf6',1,'nbtview']]],
   ['string_43',['String',['../namespacenbtview.html#a8763725f06fd8f86ba0393bf56d78761',1,'nbtview']]],
--- html/search/variables_3.js
+++ html/search/variables_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['sector_5flength_95',['SECTOR_LENGTH',['../classnbtview_1_1Region.html#a438234847870d7f6817b262fd8cf2f58',1,'nbtview::Region']]]
+  ['sector_5flength_95',['sector_length',['../classnbtview_1_1Region.html#ab7be249b44ee81041e76dc17e9f557b9',1,'nbtview::Region']]]
 ];
