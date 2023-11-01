20b465cd3d8c557a58448adf747b5d3a3172e6cd
Michael Spitznagel <exultavit@gmail.com>
2023-11-01 09:38:51 -0400

    Feat: New data model
    Compound and List are no longer type aliases for unique_ptr to STL
    container class objects.
    
    Instead Compound and List are classes that directly contain an STL map
    and vector member respectively.
    
    ** This provides much greater flexibility for any future refactoring.
    For example, if we want the Compound data to be held by a unique_ptr,
    these changes will be restricted to the Compound class implementation
    alone. **
    
    Due to this change, necessary changes to the interface and
    implementation of Tag and utility functions have been carried out as
    well.

--- nbtview/Tag.hpp
+++ nbtview/Tag.hpp
@@ -20,7 +20,8 @@
 #include <variant>
 #include <vector>
 
-//! Utilities to read, write, and manipulate NBT data
+#include "utils.hpp"
+
 namespace nbtview {
 
 class Tag;
@@ -43,6 +44,45 @@ enum class TypeCode : char {
     None = 99
 };
 
+const char *typecode_string(TypeCode type) {
+    switch (type) {
+    case TypeCode::End:
+        return "End";
+    case TypeCode::Byte:
+        return "Byte";
+    case TypeCode::Short:
+        return "Short";
+    case TypeCode::Int:
+        return "Int";
+    case TypeCode::Long:
+        return "Long";
+    case TypeCode::Float:
+        return "Float";
+    case TypeCode::Double:
+        return "Double";
+    case TypeCode::Byte_Array:
+        return "Byte_Array";
+    case TypeCode::String:
+        return "String";
+    case TypeCode::List:
+        return "List";
+    case TypeCode::Compound:
+        return "Compound";
+    case TypeCode::Int_Array:
+        return "Int_Array";
+    case TypeCode::Long_Array:
+        return "Long_Array";
+    case TypeCode::None:
+        return "None";
+    default:
+        return "Invalid TypeCode";
+    }
+}
+
+class Tag;
+
+//! placeholder type to indicate the absence of a data value
+struct None {};
 //! NBT End tag: marks the end of a Compound Tag's payload
 using End = unsigned char;
 //! 8-bit signed integer
@@ -59,202 +99,52 @@ using Float = float;
 using Double = double;
 //! array of Byte
 using Byte_Array = std::vector<Byte>;
-using Byte_Array_Ptr = std::unique_ptr<Byte_Array>;
 //! string of characters (supports UTF-8)
 using String = std::string;
-using String_Ptr = std::unique_ptr<String>;
 //! array of Tag objects
-struct List {
-    TypeCode tag_type;
-    std::vector<Tag> data;
-};
-using List_Ptr = std::unique_ptr<List>;
+class List;
 //! map from string to Tag objects
-using Compound = std::map<std::string, Tag>;
-using Compound_Ptr = std::unique_ptr<Compound>;
+class Compound;
 //! array of Int
 using Int_Array = std::vector<Int>;
-using Int_Array_Ptr = std::unique_ptr<Int_Array>;
 //! array of Long
 using Long_Array = std::vector<Long>;
-using Long_Array_Ptr = std::unique_ptr<Long_Array>;
-//! placeholder type to indicate the absence of a data value
-struct None {};
 
-using TagData = std::variant<None, End, Byte, Short, Int, Long, Float, Double,
-                             Byte_Array_Ptr, String_Ptr, List_Ptr, Compound_Ptr,
-                             Int_Array_Ptr, Long_Array_Ptr>;
-
-/**
- *  @brief Tag holds an NBT tag and provides basic access to it.
- */
-
-class Tag {
-  public:
-    // Constructors
-    Tag() : data(None()) {}
-    Tag(End x) : data(x) {}
-    Tag(Byte x) : data(x) {}
-    Tag(Short x) : data(x) {}
-    Tag(Int x) : data(x) {}
-    Tag(Long x) : data(x) {}
-    Tag(Float x) : data(x) {}
-    Tag(Double x) : data(x) {}
-    Tag(Byte_Array &&x) : data(std::make_unique<Byte_Array>(std::move(x))) {}
-    Tag(const Byte_Array &x) : data(std::make_unique<Byte_Array>(x)) {}
-    Tag(String &&x) : data(std::make_unique<String>(std::move(x))) {}
-    Tag(const String &x) : data(std::make_unique<String>(x)) {}
-    Tag(List &&x) : data(std::make_unique<List>(std::move(x))) {}
-    Tag(Compound &&x) : data(std::make_unique<Compound>(std::move(x))) {}
-    Tag(Int_Array &&x) : data(std::make_unique<Int_Array>(std::move(x))) {}
-    Tag(const Int_Array &x) : data(std::make_unique<Int_Array>(x)) {}
-    Tag(Long_Array &&x) : data(std::make_unique<Long_Array>(std::move(x))) {}
-    Tag(const Long_Array &x) : data(std::make_unique<Long_Array>(x)) {}
-
-    // TODO consider a function that returns type of an entry.
-    /**
-     *  @brief Tests whether a Compound Tag contains a particular %Tag
-     *
-     *  @param name the name of the %Tag
-     */
-    bool contains(const std::string &name) const {
-        if (!std::holds_alternative<Compound_Ptr>(data)) {
-            throw std::runtime_error(
-                "Can't get named entries from a non-Compound Tag");
-        }
-        const auto &cmpd = std::get<Compound_Ptr>(data);
-        return cmpd->find(name) != cmpd->end();
-    }
-
-    // TODO write specializations so that we can get Compound& instead of just
-    // Compound_Ptr&
-    /**
-     * @brief Gets a data entry from a Compound Tag
-     *
-     * @tparam T the type of data to be accessed.
-     * @param name the name of the element to be accessed.
-     * @return Reference to the entry's data.
-     * @throw std::runtime_error if this is not a Compound Tag.
-     * @throw std::out_of_range if no element of that name is present.
-     * @throw std::runtime_error if the element of that name is not of type T.
-     */
-    template <typename T> T &get(const std::string &name) {
-        if (!std::holds_alternative<Compound_Ptr>(data)) {
-            throw std::runtime_error(
-                "Can't get named entries from a non-Compound Tag");
-        }
-        Tag &tg = std::get<Compound_Ptr>(data)->at(name);
-        if (!std::holds_alternative<T>(tg)) {
-            throw std::runtime_error("Element " + name +
-                                     " is not of the requested type");
-        }
-        return std::get<T>(tg);
-    }
-
-    /* So, here's the design question.
-     * Do we want ease of access in navigating a compound tag hierarchy?
-     * I.e,  my_region["level"]["structures"]["starts"]    (*)
-     * This can be accomplished by using operator[] to return a Tag.
-     *
-     * Or do we want access more like:
-     * my_region.get<Compound>("level").get<Compound>("structures").get<Compound>("starts")
-     * (**)
-     *
-     * (*) is more succinct, but note that we could introduce a separator syntax
-     * to achieve similar brevity in a customized accessor:
-     *
-     * my_region.path_get("/level/structures/starts")
-     *
-     * Or maybe use '.' as a separator.  Some JSON utilities might do this for
-     * nested fields.
-     *
-     * LET'S MAKE A WISH LIST:
-     *
-     * 1> Convenient access to nested tags   [soln: path based accessor adapter]
-     * 2> Easy access to maybe/optional tags  [soln: provide a std::optional
-     * accessor layer?]  [or None tag?] 3>
-     *
-     */
-
-    /**
-     * @brief Returns the size of a container %Tag.
-     *
-     * @return Number of elements of Tag.
-     * @throw  std::runtime_error If Tag is not of a container type: Array,
-     * String, List, or Compound.
-     */
-    std::size_t size() const {
-        if (std::holds_alternative<Byte_Array_Ptr>(data)) {
-            return std::get<Byte_Array_Ptr>(data)->size();
-        } else if (std::holds_alternative<String_Ptr>(data)) {
-            return std::get<String_Ptr>(data)->size();
-        } else if (std::holds_alternative<List_Ptr>(data)) {
-            return std::get<List_Ptr>(data)->data.size();
-        } else if (std::holds_alternative<Compound_Ptr>(data)) {
-            return std::get<Compound_Ptr>(data)->size();
-        } else if (std::holds_alternative<Int_Array_Ptr>(data)) {
-            return std::get<Int_Array_Ptr>(data)->size();
-        } else if (std::holds_alternative<Long_Array_Ptr>(data)) {
-            return std::get<Long_Array_Ptr>(data)->size();
-        } else {
-            throw std::runtime_error("Called size() on a non-container tag.");
-        }
-    }
-
-    /**
-     * @brief Checks if a Tag of container type is empty.
-     *
-     * @return True if empty.
-     * @throw  std::runtime_error If Tag is not of a container type: Array,
-     * String, List, or Compound.
-     */
-    bool empty() const {
-        if (std::holds_alternative<Byte_Array_Ptr>(data)) {
-            return std::get<Byte_Array_Ptr>(data)->empty();
-        } else if (std::holds_alternative<String_Ptr>(data)) {
-            return std::get<String_Ptr>(data)->empty();
-        } else if (std::holds_alternative<List_Ptr>(data)) {
-            return std::get<List_Ptr>(data)->data.empty();
-        } else if (std::holds_alternative<Compound_Ptr>(data)) {
-            return std::get<Compound_Ptr>(data)->empty();
-        } else if (std::holds_alternative<Int_Array_Ptr>(data)) {
-            return std::get<Int_Array_Ptr>(data)->empty();
-        } else if (std::holds_alternative<Long_Array_Ptr>(data)) {
-            return std::get<Long_Array_Ptr>(data)->empty();
-        } else {
-            throw std::runtime_error("Called empty() on a non-container tag.");
-        }
-    }
-
-    // methods for list and array tags
-    /**
-     * @brief Provides access to the data of a particular entry of a List %Tag
-     * or Array %Tag.
-     *
-     * @tparam T the type of elements in the container: Byte, Int, Long, or Tag.
-     * @param index the index of the element to be accessed.
-     * @return Read/write reference to the element.
-     * @throw std::out_of_range_error If index is out of range.
-     * @throw std::runtime_error  If T is of the wrong type or the %Tag is not a
-     * container.
-     */
-    template <typename T> T &get(size_t index) {
-        if (std::holds_alternative<std::unique_ptr<std::vector<T>>>(data)) {
-            return std::get<std::unique_ptr<std::vector<T>>>(data)->at(index);
-        } else {
-            throw std::runtime_error("Mismatch in requested tag data type.");
-        }
-    }
-
-    TagData &tag_data() { return data; }
-    const TagData &tag_data() const { return data; }
-
+class List {
   private:
-    // TagData is a variant type.  Some of its alternatives are unique_ptrs.
-    // *** Invariant: We require that `data` cannot be a null pointer. ***
-    // This is so that users of Tag::tag_data() may dereference without checking
-    // for null.
-    TagData data;
+    std::vector<Tag> data;
+
+  public:
+    // inline void push_back(const Tag &value);
+    // inline Tag &operator[](size_t index);
+    void push_back(const Tag &value) { data.push_back(value); }
+    Tag &operator[](size_t index) { return data[index]; }
+    const Tag &operator[](size_t index) const { return data[index]; }
+    auto begin() { return data.begin(); }
+    const auto begin() const { return data.begin(); }
+    auto end() { return data.end(); }
+    const auto end() const { return data.end(); }
+    const std::size_t size() const { return data.size(); }
+    const bool empty() const { return data.empty(); }
+    const TypeCode list_type() const;
+};
+
+class Compound {
+  private:
+    std::map<std::string, Tag> data;
+
+  public:
+    inline void insert(const std::string &key, const Tag &value);
+    Tag &operator[](const std::string &key) { return data[key]; }
+    Tag &operator[](std::string &&key) { return data[std::move(key)]; }
+    bool contains(const std::string &key) const;
+
+    auto begin() { return data.begin(); }
+    const auto begin() const { return data.begin(); }
+    auto end() { return data.end(); }
+    const auto end() const { return data.end(); }
+    const std::size_t size() const { return data.size(); }
+    const bool empty() const { return data.empty(); }
 };
 
 //! TagID is used with std::visit to get the TypeCode of a given Tag
@@ -268,18 +158,238 @@ struct TagID {
     auto operator()(const Float &x) { return TypeCode::Float; }
     auto operator()(const Double &x) { return TypeCode::Double; }
     auto operator()(const Byte_Array &x) { return TypeCode::Byte_Array; }
-    auto operator()(const Byte_Array_Ptr &x) { return TypeCode::Byte_Array; }
     auto operator()(const String &x) { return TypeCode::String; }
-    auto operator()(const String_Ptr &x) { return TypeCode::String; }
     auto operator()(const List &x) { return TypeCode::List; }
-    auto operator()(const List_Ptr &x) { return TypeCode::List; }
     auto operator()(const Compound &x) { return TypeCode::Compound; }
-    auto operator()(const Compound_Ptr &x) { return TypeCode::Compound; }
     auto operator()(const Int_Array &x) { return TypeCode::Int_Array; }
-    auto operator()(const Int_Array_Ptr &x) { return TypeCode::Int_Array; }
     auto operator()(const Long_Array &x) { return TypeCode::Long_Array; }
-    auto operator()(const Long_Array_Ptr &x) { return TypeCode::Long_Array; }
 };
+
+class Tag {
+  public:
+    using Value =
+        std::variant<None, End, Byte, Short, Int, Long, Float, Double,
+                     Byte_Array, String, List, Compound, Int_Array, Long_Array>;
+
+  private:
+    Value value;
+
+  public:
+    template <typename T> Tag(const T &v) : value(v) {}
+    Tag() : value(None{}) {}
+
+    template <typename T> T &get() { return std::get<T>(value); }
+
+    template <typename T> const T &get() const { return std::get<T>(value); }
+
+    Value &get_value() { return value; }
+    const Value &get_value() const { return value; }
+
+    const TypeCode get_id() const { return std::visit(TagID(), value); }
+
+    template <typename T> bool is() const {
+        return std::holds_alternative<T>(value);
+    }
+    const std::size_t size() const {
+        if (std::holds_alternative<Compound>(value)) {
+            return std::get<Compound>(value).size();
+        } else if (std::holds_alternative<List>(value)) {
+            return std::get<List>(value).size();
+        }
+        throw std::runtime_error(
+            "Called size() on a Tag which is neither Compound nor List.");
+        return 0;
+    }
+    bool empty() const {
+        if (std::holds_alternative<Compound>(value)) {
+            return std::get<Compound>(value).empty();
+        } else if (std::holds_alternative<List>(value)) {
+            return std::get<List>(value).empty();
+        }
+        throw std::runtime_error(
+            "Called empty() on a Tag which is neither Compound nor List.");
+        return 0;
+    }
+
+    /**
+     *  @brief Tests whether a Compound Tag contains a particular %Tag
+     *
+     *  @param key the name of the %Tag being queried for
+     */
+    bool contains(const std::string &key) const {
+        return std::get<Compound>(value).contains(key);
+    }
+    Tag &operator[](const std::string &key) {
+        return std::get<Compound>(value)[key];
+    }
+    Tag &operator[](size_t index) { return std::get<List>(value)[index]; }
+    const Tag &operator[](size_t index) const {
+        return std::get<List>(value)[index];
+    }
+};
+
+const TypeCode List::list_type() const {
+    if (empty()) {
+        return TypeCode::End;
+    } else {
+        return data[0].get_id();
+    }
+}
+
+void Compound::insert(const std::string &key, const Tag &value) {
+    data[key] = value;
+}
+
+bool Compound::contains(const std::string &key) const {
+    return data.find(key) != data.end();
+}
+
+std::string to_string(const Tag &tag);
+
+const char *typecode_to_string(TypeCode type) {
+    switch (type) {
+    case TypeCode::End:
+        return "End";
+    case TypeCode::Byte:
+        return "Byte";
+    case TypeCode::Short:
+        return "Short";
+    case TypeCode::Int:
+        return "Int";
+    case TypeCode::Long:
+        return "Long";
+    case TypeCode::Float:
+        return "Float";
+    case TypeCode::Double:
+        return "Double";
+    case TypeCode::Byte_Array:
+        return "Byte_Array";
+    case TypeCode::String:
+        return "String";
+    case TypeCode::List:
+        return "List";
+    case TypeCode::Compound:
+        return "Compound";
+    case TypeCode::Int_Array:
+        return "Int_Array";
+    case TypeCode::Long_Array:
+        return "Long_Array";
+    case TypeCode::None:
+        return "None";
+    default:
+        return "Invalid TypeCode";
+    }
+}
+
+std::string tag_id_string(const Tag &tag) {
+    auto id = tag.get_id();
+    if (id == TypeCode::List) {
+        return tag.empty() ? "List of End"
+                           : std::string("List of ") +
+                                 typecode_to_string(tag[0].get_id());
+    } else {
+        return typecode_to_string(id);
+    }
+}
+
+template <typename T> std::string associative_to_string(const T &iterable) {
+    std::string output_string;
+    for (const auto &elt : iterable) {
+        if (output_string.empty()) {
+            output_string = "{";
+        } else {
+            output_string += ",";
+        }
+        if (snbt_requires_quoting(elt.first)) {
+            output_string += quoted_string(elt.first);
+        } else {
+            output_string += elt.first;
+        }
+
+        output_string += ":" + to_string(elt.second);
+    }
+    output_string += "}";
+    return output_string;
+}
+
+template <typename T>
+std::string sequential_to_string(const T &iterable,
+                                 const std::string &array_prefix) {
+    std::string output_string;
+    for (const auto &elt : iterable) {
+        if (output_string.empty()) {
+            output_string = "[" + array_prefix;
+        } else {
+            output_string += ",";
+        }
+        output_string += to_string(elt);
+    }
+    output_string += "]";
+    return output_string;
+}
+
+template <typename T>
+std::string array_to_string(const T &iterable, const std::string &array_prefix,
+                            const std::string &element_suffix) {
+    std::string output_string;
+    for (const auto &elt : iterable) {
+        if (output_string.empty()) {
+            output_string = "[" + array_prefix;
+        } else {
+            output_string += ",";
+        }
+        output_string += std::to_string(elt) + element_suffix;
+    }
+    output_string += "]";
+    return output_string;
+}
+
+std::string to_string(const Tag &tag) {
+    struct ToStringVisitor {
+        std::string operator()(const None x) const { return "<<NONE>>"; }
+        std::string operator()(const End x) const { return "<<END_TAG>>"; }
+        std::string operator()(const Compound &x) const {
+            return associative_to_string(x);
+        }
+        std::string operator()(const Byte x) const {
+            return std::to_string(x) + "b";
+        }
+        std::string operator()(const Short x) const {
+            return std::to_string(x) + "s";
+        }
+        std::string operator()(const Int x) const { return std::to_string(x); }
+        std::string operator()(const Long x) const {
+            return std::to_string(x) + "L";
+        }
+        std::string operator()(const Float x) const {
+            return std::to_string(x) + "f";
+        }
+        std::string operator()(const Double x) const {
+            return std::to_string(x) + "d";
+        }
+        std::string operator()(const Byte_Array &x) const {
+            return array_to_string(x, "B;", "b");
+        }
+        std::string operator()(const String &x) const {
+            return quoted_string(x);
+        }
+        std::string operator()(const List &x) const {
+            return sequential_to_string(x, "");
+        }
+        std::string operator()(const Int_Array &x) const {
+            return array_to_string(x, "I;", "");
+        }
+        std::string operator()(const Long_Array &x) const {
+            return array_to_string(x, "L;", "L");
+        }
+    };
+
+    return std::visit(ToStringVisitor{}, tag.get_value());
+}
+
+std::ostream &operator<<(std::ostream &os, const Tag &tag) {
+    return (os << to_string(tag));
+}
 
 } // namespace nbtview
 
