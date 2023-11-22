28967f6f7974e05cec3319a86d543f13910c4f87
Michael Spitznagel <exultavit@gmail.com>
2023-11-22 16:20:43 -0500

    Feat: Migration to Google Test

--- test/test_Region.cpp
+++ test/test_Region.cpp
@@ -1,32 +1,38 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-
-#include <fstream>
-#include <vector>
+#include <gtest/gtest.h>
 
 #include "Region.hpp"
 
 namespace nbt = nbtview;
 
-TEST_CASE("nbtview: Region container methods") {
+// Test for offset data
+TEST(RegionTest, OffsetData) {
     const auto filename = "test_data/r.0.0.mca";
     nbt::Region_File reg(filename);
-    SUBCASE("offset data") {
-        CHECK(reg.chunk_offset(0) == 0x02);
-        CHECK(reg.chunk_offset(1) == 0x1b);
-        CHECK(reg.chunk_offset(2) == 0x34);
-        CHECK(reg.chunk_offset(3) == 0x00);
-    }
-    SUBCASE("length data") {
-        CHECK(reg.chunk_length(0) == 0x01);
-        CHECK(reg.chunk_length(1) == 0x01);
-        CHECK(reg.chunk_length(2) == 0x01);
-        CHECK(reg.chunk_length(3) == 0x00);
-    }
-    SUBCASE("timestamp data") {
-        CHECK(reg.chunk_timestamp(0) == 0x600a1b75);
-        CHECK(reg.chunk_timestamp(1) == 0x6004c6d7);
-        CHECK(reg.chunk_timestamp(2) == 0x6004c6d7);
-        CHECK(reg.chunk_timestamp(3) == 0x00000000);
-    }
+
+    EXPECT_EQ(reg.chunk_offset(0), 0x02);
+    EXPECT_EQ(reg.chunk_offset(1), 0x1b);
+    EXPECT_EQ(reg.chunk_offset(2), 0x34);
+    EXPECT_EQ(reg.chunk_offset(3), 0x00);
+}
+
+// Test for length data
+TEST(RegionTest, LengthData) {
+    const auto filename = "test_data/r.0.0.mca";
+    nbt::Region_File reg(filename);
+
+    EXPECT_EQ(reg.chunk_length(0), 0x01);
+    EXPECT_EQ(reg.chunk_length(1), 0x01);
+    EXPECT_EQ(reg.chunk_length(2), 0x01);
+    EXPECT_EQ(reg.chunk_length(3), 0x00);
+}
+
+// Test for timestamp data
+TEST(RegionTest, TimestampData) {
+    const auto filename = "test_data/r.0.0.mca";
+    nbt::Region_File reg(filename);
+
+    EXPECT_EQ(reg.chunk_timestamp(0), 0x600a1b75);
+    EXPECT_EQ(reg.chunk_timestamp(1), 0x6004c6d7);
+    EXPECT_EQ(reg.chunk_timestamp(2), 0x6004c6d7);
+    EXPECT_EQ(reg.chunk_timestamp(3), 0x00000000);
 }
--- test/test_bigtest.cpp
+++ test/test_bigtest.cpp
@@ -1,97 +1,76 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
+#include <gtest/gtest.h>
 
 #include <fstream>
-#include <variant>
 #include <vector>
 
 #include "Tag.hpp"
 #include "nbtview.hpp"
 
-namespace nbt = nbtview;
+using namespace nbtview;
 
-TEST_CASE("nbtview: bigtest.nbt values") {
-    const auto filename = "test_data/bigtest.nbt";
-    std::ifstream bigtest_stream(filename);
-    auto [root_name, root_tag] = nbt::read_binary(bigtest_stream);
-    CHECK(root_tag.is<nbt::Compound>());
-    CHECK(root_name == "Level");
+class BigTest : public ::testing::Test {
+  protected:
+    Tag root_tag;
+    std::string root_name;
 
-    SUBCASE("byteTest") {
-        CHECK(root_tag.contains("byteTest"));
-        CHECK(root_tag["byteTest"].get<nbt::Byte>() == 127);
+    virtual void SetUp() {
+        const auto filename = "test_data/bigtest.nbt";
+        std::ifstream bigtest_stream(filename);
+        std::tie(root_name, root_tag) = read_binary(bigtest_stream);
     }
-    SUBCASE("longTest") {
-        CHECK(root_tag.contains("longTest"));
-        CHECK(root_tag["longTest"].get<nbt::Long>() == 9223372036854775807L);
-    }
-    SUBCASE("shortTest") {
-        CHECK(root_tag.contains("shortTest"));
-        CHECK(root_tag["shortTest"].get<nbt::Short>() == 32767);
-    }
-    SUBCASE("stringTest") {
-        CHECK(root_tag.contains("stringTest"));
-        CHECK(root_tag["stringTest"].get<nbt::String>() ==
+};
+
+TEST_F(BigTest, Values) {
+    EXPECT_TRUE(root_tag.is<nbtview::Compound>());
+    EXPECT_EQ(root_name, "Level");
+    EXPECT_EQ(root_tag["byteTest"].get<Byte>(), 127);
+    EXPECT_EQ(root_tag["longTest"].get<Long>(), 9223372036854775807L);
+    EXPECT_EQ(root_tag["shortTest"].get<Short>(), 32767);
+    EXPECT_EQ(root_tag["stringTest"].get<String>(),
               "HELLO WORLD THIS IS A TEST STRING ÅÄÖ!");
-    }
-    SUBCASE("floatTest") {
-        CHECK(root_tag.contains("floatTest"));
-        CHECK(root_tag["floatTest"].get<nbt::Float>() ==
-              doctest::Approx(0.49823147));
-    }
-    SUBCASE("intTest") {
-        CHECK(root_tag.contains("intTest"));
-        CHECK(root_tag["intTest"].get<nbt::Int>() == 2147483647);
-    }
-    SUBCASE("doubleTest") {
-        CHECK(root_tag.contains("doubleTest"));
-        CHECK(root_tag["doubleTest"].get<nbt::Double>() ==
-              doctest::Approx(0.4931287132182315));
-    }
-    SUBCASE("nested compound test") {
-        CHECK(root_tag.contains("nested compound test"));
-        auto &nested = root_tag["nested compound test"];
-        SUBCASE("egg") {
-            auto &egg = nested["egg"];
-            CHECK(egg["name"].get<nbt::String>() == "Eggbert");
-            CHECK(egg["value"].get<nbt::Float>() == doctest::Approx(0.5));
-        }
-        SUBCASE("ham") {
-            auto &ham = nested["ham"];
-            CHECK(ham["name"].get<nbt::String>() == "Hampus");
-            CHECK(ham["value"].get<nbt::Float>() == doctest::Approx(0.75));
-        }
-    }
-    SUBCASE("list test (long)") {
-        auto &list_long = root_tag["listTest (long)"].get<nbt::List>();
-        CHECK(nbt::list_type(list_long) == nbt::TypeCode::Long);
-        REQUIRE(list_long.size() == 5);
-        auto longvals = std::vector<nbt::Long>{11, 12, 13, 14, 15};
-        for (std::size_t i = 0; i < list_long.size(); ++i) {
-            CHECK(list_long[i].get<nbt::Long>() == longvals[i]);
-        }
-    }
-    SUBCASE("list test (compound)") {
-        auto &list_cmpd = root_tag["listTest (compound)"].get<nbt::List>();
-        CHECK(nbt::list_type(list_cmpd) == nbt::TypeCode::Compound);
-        REQUIRE(list_cmpd.size() == 2);
-        auto &cmpd0 = list_cmpd[0].get<nbt::Compound>();
-        CHECK(cmpd0["created-on"].get<nbt::Long>() == 1264099775885L);
-        CHECK(cmpd0["name"].get<nbt::String>() == "Compound tag #0");
-        auto &cmpd1 = list_cmpd[1].get<nbt::Compound>();
-        CHECK(cmpd1["created-on"].get<nbt::Long>() == 1264099775885L);
-        CHECK(cmpd1["name"].get<nbt::String>() == "Compound tag #1");
-    }
-    SUBCASE("byteArrayTest") {
-        auto byte_array_name = "byteArrayTest "
-                               "(the first 1000 values of (n*n*255+n*7)%100, "
-                               "starting with n=0 (0, 62, 34, 16, 8, ...))";
+    EXPECT_NEAR(root_tag["floatTest"].get<Float>(), 0.49823147, 1e-5);
+    EXPECT_EQ(root_tag["intTest"].get<Int>(), 2147483647);
+    EXPECT_NEAR(root_tag["doubleTest"].get<Double>(), 0.4931287132182315, 1e-9);
+}
 
-        CHECK(root_tag.contains(byte_array_name));
-        auto &byte_array = root_tag[byte_array_name].get<nbt::Byte_Array>();
-        REQUIRE(byte_array.size() == 1000);
-        for (int n = 0; n < 1000; ++n) {
-            CHECK(byte_array[n] == (n * n * 255 + n * 7) % 100);
-        }
+TEST_F(BigTest, NestedCompounds) {
+    auto &nested = root_tag["nested compound test"];
+    EXPECT_EQ(nested["egg"]["name"].get<String>(), "Eggbert");
+    EXPECT_NEAR(nested["egg"]["value"].get<Float>(), 0.5, 1e-9);
+    EXPECT_EQ(nested["ham"]["name"].get<String>(), "Hampus");
+    EXPECT_NEAR(nested["ham"]["value"].get<Float>(), 0.75, 1e-9);
+}
+
+TEST_F(BigTest, ListOfLongs) {
+    auto &list_long = root_tag["listTest (long)"].get<nbtview::List>();
+    EXPECT_EQ(list_type(list_long), TypeCode::Long);
+    EXPECT_EQ(list_long.size(), 5);
+    auto longvals = std::vector<Long>{11, 12, 13, 14, 15};
+    for (std::size_t i = 0; i < list_long.size(); ++i) {
+        EXPECT_EQ(list_long[i].get<Long>(), longvals[i]);
+    }
+}
+
+TEST_F(BigTest, ListOfCompounds) {
+    auto &list_cmpd = root_tag["listTest (compound)"].get<nbtview::List>();
+    EXPECT_EQ(list_type(list_cmpd), TypeCode::Compound);
+    EXPECT_EQ(list_cmpd.size(), 2);
+    auto &cmpd0 = list_cmpd[0].get<nbtview::Compound>();
+    EXPECT_EQ(cmpd0["created-on"].get<Long>(), 1264099775885L);
+    EXPECT_EQ(cmpd0["name"].get<String>(), "Compound tag #0");
+    auto &cmpd1 = list_cmpd[1].get<nbtview::Compound>();
+    EXPECT_EQ(cmpd1["created-on"].get<Long>(), 1264099775885L);
+    EXPECT_EQ(cmpd1["name"].get<String>(), "Compound tag #1");
+}
+
+TEST_F(BigTest, ByteArray) {
+    auto byte_array_name = "byteArrayTest "
+                           "(the first 1000 values of (n*n*255+n*7)%100, "
+                           "starting with n=0 (0, 62, 34, 16, 8, ...))";
+    EXPECT_TRUE(root_tag.contains(byte_array_name));
+    auto &byte_array = root_tag[byte_array_name].get<Byte_Array>();
+    EXPECT_EQ(byte_array.size(), 1000);
+    for (int n = 0; n < 1000; ++n) {
+        EXPECT_EQ(byte_array[n], (n * n * 255 + n * 7) % 100);
     }
 }
